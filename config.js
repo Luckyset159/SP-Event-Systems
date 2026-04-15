f2f9c1d98a7166080d2155a2552a5625fe73f48d
Luckyset159 <luckyset@gmail.com>
2026-04-15 18:13:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxRIZx-d26-e-NcJKhHw2pdBAf0J5Seubr6M-enAH9FzTxfCg-_VLXNaqIPMnhLlr5Ivg/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyjrKulrkVbT5O74LFwhL4aBi5qS1DvMUt-D9P7kjubfqTygvTnkF5YZ3Eh56950ommCA/exec'
 };
